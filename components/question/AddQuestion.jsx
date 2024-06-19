7366371d8f530bd87685a54dd9ecfbbd7585eded
Darya Yatchanka <darya.yatchanka@gmail.com>
2024-06-19 15:06:45 -0400

    AddQuestion +buttons

--- components/question/AddQuestion.jsx
+++ components/question/AddQuestion.jsx
@@ -9,6 +9,10 @@ const AddQuestion = () =>{
     const[newSubject, setNewSubject] = useState("")
     const[subjectOptions, setSubjectOptions]= useState([""])
 
+    useEffect(() => {
+        fetchSubjects()
+    }, [])
+
 const fetchSubjects = async() =>{
     try {
         const subjectData = await getSubjects()
@@ -41,7 +45,7 @@ const handleCorrectAnswerChange = (index, value) =>{
 
 }
 
-const handleCorrectAnswer = () =>{
+    const handleAddCorrectAnswer = () => {
     setCorrectAnswers([...correctAnswers, ""])
 }
 
@@ -86,8 +90,167 @@ const handleAddSubject = () =>{
 }
 
     return(
-        <div>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div classname="col-md-6 mt-5">
+                    <div className="card">
+                        <div className="card-header">
+                            <h5 className="card-title">Add New Question</h5>
+                        </div>
+                        <div className="card-body">
+                            <form onSubmit={handleSubmit} className="p-2">
+                                <div className="mb-3">
+                                    <label htmlFor="subject" className="form-label text-info">
+                                    Select a subject
+                                    </label>
+                                    <select
+                                        id="subject"
+                                        value={subject}
+                                        onChange={(e) => setSubject(e.target.value)}
+                                        className="from-control">
+                                        <option value="">Select a Subject</option>
+                                        <option value={"New"}>Add New <Subject></Subject></option>
+                                        {subjectOptions.map((option) => (
+                                            <option key={option} value={option}>
+                                                {option}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                {subject === "New" && (
+                                    <div className="mb-3">
+                                        <label htmlFor="new-subject" className="for-label text-info">
+                                            Add a New Subject
+                                        </label>
+                                        <input
+                                            type="text"
+                                            id="new-subject"
+                                            value={newSubject}
+                                            onChange={(event) => setNewSubject(e.target.value)}
+                                            className="form-control"
+                                        />
+                                        <button
+                                            type="button"
+                                            className="btn btn-outline-primary btn-sm mt-2"
+                                            onClick={handleAddSubject}
+                                        >Add Subject
+                                        </button>
+                                    </div>
+                                )}
+                                <div className="mb-3">
+                                    <label htmlFor="question-text" classname="form-label text-info">
+                                        Question
+                                    </label>
+                                    <textarea
+                                        classname="form-control"
+                                        rows={4}
+                                        value={question}
+                                        onChange={(e) => setQuestionText(e.target.value)}
+                                    >
+                                    </textarea>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="question-type" classname="form-label text-info">
+                                        Question Type
+                                    </label>
+                                    <select 
+                                        id="question-type"
+                                        value={questionType}
+                                        onChange={(event) => setQuestionType(event.target.value)} 
+                                        className="form-control">
+                                        <option value={"single"}>Single Answer</option>
+                                        <option value={"multiple"}>Multiple Answers</option>
+                                    </select>
+                                </div>
+                                <div className= "mb-3">
+                                <label htmlFor="choices" classname="form-label text-primary">
+                                    Choices
+                                </label>
+                                {choices.map((choice, index) => (
+                                    <div key={index} className="input-group mb-3">
+                                        <input
+                                            type="text"
+                                            value={choice}
+                                            onChange={(e) => handleChoiceChange(index, e.target.value)}
+                                            className="form-control"
+                                        />
+                                        <button
+                                            type="button"
+                                            onClick={() => handleRemoveChoice(index)}
+                                            className="btn btn-outline-danger btn-sm"
+                                        >
+                                            Remove
+                                        </button>
+                                    </div>
+                                ))}
+                                <button
+                                    type="button"
+                                    onClick={handleAddChoice}
+                                    className="btn btn-outline-danger btn-sm"
+                                >
+                                    Add Choice
+                                </button>
+                                </div>
+                                {questionType === "single" && (
+                                    <div className="mb-3">
+                                        <label htmlFor="answer" className="form-label text-info">
+                                            Correct Answer
+                                        </label>
+                                        <input
+                                            type="text"
+                                            id="answer"
+                                            value={correctAnswers[0]}
+                                            onChange={(e) => handleCorrectAnswerChange(0, e.target.value)}
+                                            className="form-control"
+                                        />
+                                    </div>
+                                )}
+                                {questionType === "multiple" && (
+                                    <div className="mb-3">
+                                        <label htmlFor="answer" className="form-label text-info">
+                                            Correct Answer(s)
+                                        </label>
+                                        {correctAnswers.map((answer, index) => (
+                                        <div key={index} className="d-flex mb-2">
+                                            <input
+                                                type="text"
+                                                value={answer}
+                                                onChange={(e) => handleCorrectAnswerChange(index, e.target.value)}
+                                                className="form-control"
 
+                                            />
+                                            {index > 0 && (
+                                                <button
+                                                    type="button"
+                                                    className="btn btn-danger btm-sm"
+                                                    onClick={() => handleRemoveCorrectAnswer(index)}>
+                                                    Remove
+                                                </button>
+                                            )}
+                                        </div>
+                                    ))}
+                                        <button
+                                            type="button"
+                                            className= "btn btn-outline-info"
+                                            onClick={handleAddCorrectAnswer}>
+                                            Add Correct Answer
+                                        </button>
+                                    </div>
+                                )}
+                                {!correctAnswer.length && <p>Please select at least one correct answer.</p>}
+                                <div className="btn-group">
+                                    <button type="submit" className="btn btn-outline-success mr-2">
+                                        Save Question
+                                    </button>
+                                    {/* <Link to=("") className="btn btn-outline-success mr-2">
+                                    Save Question
+                                </Link> */}
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
